a101f50503b442dc75aef8a147c6819e09c9d6d6
bnohad <bnohad@gmail.com>
2017-03-28 17:16:48 +0300

    Exposed DNS Resolver IP in the config file, renamed pipeline

--- docker-compose/ls/entrypoint.sh
+++ docker-compose/ls/entrypoint.sh
@@ -4,7 +4,6 @@
 xpack_string=xpack.monitoring.enabled
 config_file="$LS_PATH/config/logstash.yml"
 
-
 if grep -q "$xpack_string" "$config_file"
 then
   sed -i "s/$xpack_string: true/$xpack_string: false/g" "$config_file"
